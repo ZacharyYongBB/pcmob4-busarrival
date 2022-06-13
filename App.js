1571ddd3949cc8c5c3f102a4acf76e66ff367291
ZacharyYongBB <zacharyyong_1997@outlook.com>
2022-06-13 11:50:30 +0800

    add next next bus

--- App.js
+++ App.js
@@ -7,6 +7,7 @@ import React, { useState , useEffect } from 'react';
 export default function App() {
   const [loading,setLoading] = useState(true);
   const [arrival,setArrival] = useState("");
+  const [nextArrival,setNextArrival] = useState("");
 
   const BUSSTOP_URL = "https://arrivelah2.busrouter.sg/?id=22249"
 
@@ -22,10 +23,13 @@ export default function App() {
       const myBus = responseData.services.filter(
         (item) => item.no === "192"
         )[0];
+      const myNextBus = responseData.services.filter(
+        (item) => item.no === "192"
+        )[0];
         console.log("My Bus:");
         console.log(myBus.next.time);
         setArrival(myBus.next.time);
-        
+        setNextArrival(myNextBus.next2.time);
         setLoading(false);
     });
   }
@@ -39,10 +43,18 @@ export default function App() {
 
   return (
     <View style={styles.container}>
-      <Text style={styles.title}>Bus arrival time: </Text>
+      <Text style={styles.bigTitle}>BUS 192</Text>
+      <Text style={styles.title}>NEXT Bus arrival time: </Text>
       <Text style={styles.arrivalTime}>{loading ? <ActivityIndicator color={'blue'} size={"large"}/> : arrival} </Text>
-      <TouchableOpacity  style={styles.button} onPress={() => setLoading(false)}>
+
+      <Text style={styles.title}>NEXT NEXT Bus arrival time: </Text>
+      <Text style={styles.arrivalTime}>{loading ? <ActivityIndicator color={'blue'} size={"large"}/> : nextArrival} </Text>
+
+      <TouchableOpacity  style={styles.button} onPress={() => loadBusStopData()}>
         <Text style={styles.buttonText}>Refresh</Text>
+      </TouchableOpacity>
+      <TouchableOpacity  style={styles.button} onPress={() => setLoading(true)}>
+        <Text style={styles.buttonText}>STOP</Text>
       </TouchableOpacity>
 
       <StatusBar style="auto" />
@@ -53,7 +65,7 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: 'lightgreen',
     alignItems: 'center',
     justifyContent: 'center',
     fontSize: 100,
@@ -63,11 +75,26 @@ const styles = StyleSheet.create({
     marginVertical: 20,
   },
   button: {
-    backgroundColor:"#f0f",
+    backgroundColor:"green",
     padding: 20,
     marginVertical:20,
+    borderRadius:10,
+    marginTop:20,
   },
   buttonText: {
     fontSize:20,
+    color:'white',
+    
+  },
+  title: {
+    marginTop:30,
+    fontSize:30,
+  },
+  arrivalTime:{
+    marginTop:5,
+    fontSize:20,
+  },
+  bigTitle: {
+    fontSize:40,
   },
 });
