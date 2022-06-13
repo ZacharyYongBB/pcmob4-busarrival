e1e243a948458801f88855fffd48cf333449bdfc
ZacharyYongBB <zacharyyong_1997@outlook.com>
2022-06-13 10:55:33 +0800

    Add title

--- App.js
+++ App.js
@@ -1,10 +1,23 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, TouchableOpacity,  ActivityIndicator } from 'react-native';
+import React, { useState } from 'react';
+
 
 export default function App() {
+  const [loading,setLoading] = useState(true);
+
+  const BUSSTOP_URL = "https://arrivelah2.busrouter.sg/?id=22249"
+
+
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text style={styles.title}>Bus arrival time: </Text>
+      <Text style={styles.arrivalTime}>{loading ? <ActivityIndicator color={'blue'}/> : "Loaded"} </Text>
+      <TouchableOpacity  style={styles.button} onPress={() => setLoading(true)}>
+        <Text style={styles.buttonText}>Refresh</Text>
+      </TouchableOpacity>
+
       <StatusBar style="auto" />
     </View>
   );
@@ -16,5 +29,18 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+    fontSize: 100,
+  },
+  text: {
+    fontSize: 50,
+    marginVertical: 20,
+  },
+  button: {
+    backgroundColor:"#f0f",
+    padding: 20,
+    marginVertical:20,
+  },
+  buttonText: {
+    fontSize:20,
   },
 });
