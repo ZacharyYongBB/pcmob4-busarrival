42f3deab2ecd24eb24740fa48361d095a6cfdf39
ZacharyYongBB <zacharyyong_1997@outlook.com>
2022-06-13 12:12:33 +0800

    added next bus countdown

--- App.js
+++ App.js
@@ -8,6 +8,8 @@ export default function App() {
   const [loading,setLoading] = useState(true);
   const [arrival,setArrival] = useState("");
   const [nextArrival,setNextArrival] = useState("");
+  const [countdown,setCountdown] = useState("");
+  const [nextCountdown,setNextCountdown] = useState("");
 
   const BUSSTOP_URL = "https://arrivelah2.busrouter.sg/?id=22249"
 
@@ -20,16 +22,21 @@ export default function App() {
     .then((responseData) => {
       console.log("Original Data:");
       console.log(responseData);
-      const myBus = responseData.services.filter(
-        (item) => item.no === "192"
-        )[0];
-      const myNextBus = responseData.services.filter(
+
+        const myBus = responseData.services.filter(
         (item) => item.no === "192"
         )[0];
         console.log("My Bus:");
         console.log(myBus.next.time);
-        setArrival(myBus.next.time);
-        setNextArrival(myNextBus.next2.time);
+
+          setArrival(myBus.next.time);
+          setCountdown(myBus.next.duration_ms);
+          
+          
+          setNextArrival(myBus.next2.time);
+          setNextCountdown(myBus.next2.duration_ms);
+          
+
         setLoading(false);
     });
   }
@@ -46,9 +53,11 @@ export default function App() {
       <Text style={styles.bigTitle}>BUS 192</Text>
       <Text style={styles.title}>NEXT Bus arrival time: </Text>
       <Text style={styles.arrivalTime}>{loading ? <ActivityIndicator color={'blue'} size={"large"}/> : arrival} </Text>
+      <Text style={styles.arrivalTime}>{loading ? <ActivityIndicator color={'blue'} size={"large"}/> : countdown} </Text>
 
       <Text style={styles.title}>NEXT NEXT Bus arrival time: </Text>
       <Text style={styles.arrivalTime}>{loading ? <ActivityIndicator color={'blue'} size={"large"}/> : nextArrival} </Text>
+      <Text style={styles.arrivalTime}>{loading ? <ActivityIndicator color={'blue'} size={"large"}/> : nextCountdown} </Text>
 
       <TouchableOpacity  style={styles.button} onPress={() => loadBusStopData()}>
         <Text style={styles.buttonText}>Refresh</Text>
