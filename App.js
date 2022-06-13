985e624416bc2ebe95093ea2d54e40d00c4472d0
ZacharyYongBB <zacharyyong_1997@outlook.com>
2022-06-13 11:25:28 +0800

    add autorefresh for arrival time

--- App.js
+++ App.js
@@ -1,20 +1,47 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View, TouchableOpacity,  ActivityIndicator } from 'react-native';
-import React, { useState } from 'react';
+import React, { useState , useEffect } from 'react';
+
 
 
 export default function App() {
   const [loading,setLoading] = useState(true);
+  const [arrival,setArrival] = useState("");
 
   const BUSSTOP_URL = "https://arrivelah2.busrouter.sg/?id=22249"
+
+  function loadBusStopData() {
+    setLoading(true);
+    fetch(BUSSTOP_URL)
+    .then((response) => {
+      return response.json();
+    })
+    .then((responseData) => {
+      console.log("Original Data:");
+      console.log(responseData);
+      const myBus = responseData.services.filter(
+        (item) => item.no === "192"
+        )[0];
+        console.log("My Bus:");
+        console.log(myBus.next.time);
+        setArrival(myBus.next.time);
+        
+        setLoading(false);
+    });
+  }
+
+  useEffect(() => {
+    const interval = setInterval(loadBusStopData,5000);
+    return () => clearInterval(interval);
+  },[]);
 
 
 
   return (
     <View style={styles.container}>
       <Text style={styles.title}>Bus arrival time: </Text>
-      <Text style={styles.arrivalTime}>{loading ? <ActivityIndicator color={'blue'}/> : "Loaded"} </Text>
-      <TouchableOpacity  style={styles.button} onPress={() => setLoading(true)}>
+      <Text style={styles.arrivalTime}>{loading ? <ActivityIndicator color={'blue'} size={"large"}/> : arrival} </Text>
+      <TouchableOpacity  style={styles.button} onPress={() => setLoading(false)}>
         <Text style={styles.buttonText}>Refresh</Text>
       </TouchableOpacity>
 
