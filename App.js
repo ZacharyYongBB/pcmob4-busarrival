60863e3095a9a40133359200d42bcbe6515e0b3e
ZacharyYongBB <zacharyyong_1997@outlook.com>
2022-06-14 10:32:19 +0800

    added bus number and bus stop input

--- App.js
+++ App.js
@@ -1,5 +1,5 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View, TouchableOpacity,  ActivityIndicator } from 'react-native';
+import { StyleSheet, Text, View, TouchableOpacity,  ActivityIndicator, TextInput,SafeAreaView } from 'react-native';
 import React, { useState , useEffect } from 'react';
 
 
@@ -16,8 +16,10 @@ export default function App() {
   const timeArrival = new Date(arrival);
   const timeNextArrival = new Date(nextArrival);
  
+  const [busNumber, onChangeBus] = React.useState(null); 
+  const [stopNumber, onChangeStop] = React.useState(null); 
 
-  const BUSSTOP_URL = "https://arrivelah2.busrouter.sg/?id=22249"
+  const BUSSTOP_URL = `https://arrivelah2.busrouter.sg/?id=${stopNumber}`
 
 
   function millisToMinutesAndSeconds(millis) {
@@ -41,7 +43,7 @@ export default function App() {
       console.log(responseData);
 
         const myBus = responseData.services.filter(
-        (item) => item.no === "192"
+        (item) => item.no === busNumber
         )[0];
         console.log("My Bus:");
         console.log(myBus.next.time);
@@ -59,15 +61,36 @@ export default function App() {
   }
 
   useEffect(() => {
-    const interval = setInterval(loadBusStopData,5000);
+    const interval = setInterval(loadBusStopData,15000);
     return () => clearInterval(interval);
   },[]);
 
 
 
   return (
-    <View style={styles.container}>
-      <Text style={styles.bigTitle}>BUS 192</Text>
+    <SafeAreaView style={styles.container}>
+      <Text style={styles.bigTitle}>Bus Number</Text>
+      <TextInput
+      style={styles.input}
+      onChangeText={onChangeBus}
+      value={busNumber}
+      placeholder="Bus Number"
+      keyboardType="numeric"
+       />
+      <Text style={styles.bigTitle}>Stop Number</Text>
+      <TextInput
+      style={styles.input}
+      onChangeText={onChangeStop}
+      value={stopNumber}
+      placeholder="Bus Stop Number"
+      keyboardType="numeric"
+      />
+
+      <TouchableOpacity  style={styles.button} onPress={() => loadBusStopData()}>
+        <Text style={styles.buttonText}>Search</Text>
+      </TouchableOpacity>
+      
+      
       <Text style={styles.title}>NEXT Bus arrival time: </Text>
       <Text style={styles.arrivalTime}>{loading ? <ActivityIndicator color={'blue'} size={"large"}/> : timeArrival.toTimeString()} </Text>
       <Text style={styles.arrivalTime}>{loading ? <ActivityIndicator color={'blue'} size={"large"}/> : `coming in: ${timeCountdown}`} </Text>
@@ -84,7 +107,7 @@ export default function App() {
       </TouchableOpacity>
 
       <StatusBar style="auto" />
-    </View>
+    </SafeAreaView>
   );
 }
 
@@ -114,13 +137,22 @@ const styles = StyleSheet.create({
   },
   title: {
     marginTop:30,
-    fontSize:30,
+    fontSize:25,
   },
   arrivalTime:{
     marginTop:5,
     fontSize:20,
+    color:'gray',
   },
   bigTitle: {
-    fontSize:40,
+    fontSize:30,
+  },
+  input: {
+    height: 40,
+    margin: 12,
+    borderWidth: 1,
+    padding: 10,
+    backgroundColor:'white',
+    borderRadius:5,
   },
 });
